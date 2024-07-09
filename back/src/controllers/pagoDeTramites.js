5705d2ab83a609159ccedc034a4783ace1b69ece
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-07-08 22:08:11 -0600

    Imprimir opciones de alumnos

--- back/src/controllers/pagoDeTramites.js
+++ back/src/controllers/pagoDeTramites.js
@@ -94,7 +94,7 @@ const db = mysql.createConnection({
   //probar los demás parametros para busqueda
   exports.buscarORfiltrarTramites = [/*authenticateJWT,*/ (req, res) => {
     const {folio_busqueda, concepto_busqueda,nombreBusqueda, apellido_p_busqueda, apellido_m_busqueda, fechaDeCorteFiltro, estatusFiltro, gradoFiltro, grupoFiltro} = req.body;
-    let consulta = `SELECT Alumnos.nombre, Alumnos.apellido_p, Alumnos.apellido_m, Alumnos.grado, Alumnos.grupo, PagoTramites.id, PagoTramites.folio, PagoTramites.concepto, PagoTramites.monto, PagoTramites.fechaDeCorte, EstatusPago.tipo_estatus 
+    let consulta = `SELECT PagoTramites.id, PagoTramites.folio, Alumnos.nombre, Alumnos.apellido_p, Alumnos.apellido_m, Alumnos.grado, Alumnos.grupo, PagoTramites.concepto, PagoTramites.monto, PagoTramites.fechaDeCorte, EstatusPago.tipo_estatus 
       FROM PagoTramites
       JOIN EstatusPago ON PagoTramites.id_estatus = EstatusPago.id
       JOIN Alumnos ON PagoTramites.id_alumno = Alumnos.id
@@ -147,4 +147,36 @@ const db = mysql.createConnection({
       }
     )
     
+  }];
+
+  exports.printOptionsAlumnos = [(req, res) => {
+    const {nombre_busqueda, apellido_p_busqueda, apellido_m_busqueda, noControl_busqueda} = req.body;
+    let consulta = `SELECT Alumnos.id, Alumnos.noControl, Alumnos.nombre, Alumnos.apellido_p, Alumnos.apellido_m, Alumnos.grado, Alumnos.grupo
+      FROM Alumnos WHERE 1 = 1`;
+    let parametros = [];
+
+    if(nombre_busqueda){
+      consulta += `AND Alumnos.nombre LIKE ?`;
+      parametros.push(nombre_busqueda+'%');
+    }
+    if(apellido_p_busqueda){
+      consulta += `AND Alumnos.apellido_p LIKE ?`;
+      parametros.push(apellido_m_busqueda+'%');
+    }
+    if(apellido_m_busqueda){
+      consulta += `AND Alumnos.apellido_m LIKE ?`;
+      parametros.push(apellido_m_busqueda+'%');
+    }
+    if(noControl_busqueda){
+      consulta += `AND Alumnos.noCOntrol = ?`;
+      parametros.push(noControl_busqueda);
+    }
+
+    db.query(consulta, parametros, (err, result) => {
+      if (err){
+        res.status(500).send(`Error al imprimir las opciones de alumnos`);
+        throw err;
+      }
+      res.json(result);
+    });
   }];
